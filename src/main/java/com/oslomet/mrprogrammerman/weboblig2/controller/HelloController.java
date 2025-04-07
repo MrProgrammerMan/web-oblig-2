42803df27ac7a6aee5c8ae1809b24a8bf1299a3b
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-04-07 13:40:31 +0200

    chore(setup): set up maven project, ci pipeline, set up spring boot main.

---
+++ src/main/java/com/oslomet/mrprogrammerman/weboblig2/controller/HelloController.java
@@ -0,0 +1,4 @@
+package com.oslomet.mrprogrammerman.weboblig2.controller;
+
+public class HelloController {
+}
---
+++ src/test/java/com/oslomet/mrprogrammerman/weboblig2/WebAppTests.java
@@ -0,0 +1,13 @@
+package com.oslomet.mrprogrammerman.weboblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void testsRun() {
+        System.out.println("It works!");
+    }
+}
